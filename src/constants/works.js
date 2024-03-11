9ac93633ce6b8ba97e3031d2c5a002a293d02851
Paras <parasmahla80@gmail.com>
2024-03-11 15:14:24 +0530

    updated url in  works

--- src/constants/works.js
+++ src/constants/works.js
@@ -3,7 +3,7 @@ import site from "../images/works/site.png"
 
 
 const web = [
-    {title:"Personal website", image: site, description:"My portfolio website", stack: "React.js, Tailwind CSS, Vite", duration: 0.13, link: "https://github.com/Georgelafayen/Personal-website"},
+    {title:"Personal website", image: site, description:"My portfolio website", stack: "React.js, Tailwind CSS, Vite", duration: 0.13, link: "https://github.com/parasmahla1/portfolio"},
 
 ]
 
