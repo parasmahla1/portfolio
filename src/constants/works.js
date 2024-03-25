0fa0eaab87552b7c71fbbe0cc68056e78ce42bed
Paras <parasmahla80@gmail.com>
2024-03-25 14:08:39 +0530

    updated  projects

--- src/constants/works.js
+++ src/constants/works.js
@@ -1,10 +1,12 @@
 
 import site from "../images/works/site.png"
-
-
+import yt from "../images/works/yt.png"
+import chatApp2 from "../images/works/chatApp2.png"
 const web = [
-    {title:"Personal website", image: site, description:"My portfolio website", stack: "React.js, Tailwind CSS, Vite", duration: 0.13, link: "https://github.com/parasmahla1/portfolio"},
 
+    {title:"Personal website", image: site, description:"My portfolio website", stack: "React.js, Tailwind CSS, Vite", duration: 0.13, link: "https://github.com/parasmahla1/portfolio"},
+    {title:"Chat App", image: chatApp2, description:"Real Time Chat Application", stack: "React.js, Tailwind CSS, Vite, DaisyUI, MongoDB, Express, Socket.io", duration: 0.15, link: "https://github.com/parasmahla1/chat-app"},
+    {title:"Youtube Clone", image: yt, description:"Youtube build using Youtube API", stack: "React.js,CSS, Material UI, Rapid API", duration: 0.2, link: "https://github.com/parasmahla1/yt-clone"},
 ]
 
 
